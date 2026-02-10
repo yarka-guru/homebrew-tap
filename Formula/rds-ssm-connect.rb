5244b09f890613d85e9be9674152adce5743d8e7
Iaroslav Pyrogov <78590208+yarka-guru@users.noreply.github.com>
2026-02-10 22:29:42 +0100

    Update rds-ssm-connect to 1.7.12

--- Formula/rds-ssm-connect.rb
+++ Formula/rds-ssm-connect.rb
@@ -1,18 +1,13 @@
 class RdsSsmConnect < Formula
   desc "Secure database tunneling via AWS SSM"
   homepage "https://github.com/yarka-guru/connection_app"
-  version "1.7.5"
+  version "1.7.12"
   license "MIT"
 
   on_linux do
     on_intel do
       url "https://github.com/yarka-guru/connection_app/releases/download/v#{version}/RDS.SSM.Connect_#{version}_amd64.deb"
-      sha256 "d0e622681deff5290734370e6b4f8c1d3d5c45ae394fdbcddaf9b5d05e5c2ab6"
-    end
-
-    on_arm do
-      url "https://github.com/yarka-guru/connection_app/releases/download/v#{version}/RDS.SSM.Connect_#{version}_arm64.deb"
-      sha256 "6f0ec22153187cbaa67ea6cde298d7444e237f29bbf79a93c26133ccb9d391cb"
+      sha256 "bf044df9baef760343b9797f4a806d3a5af06472719e7dc083f397d38ab54d3d"
     end
   end
 
