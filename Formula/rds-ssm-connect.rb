10bfd89ca163eb702e9ef22c258ff10acc263af6
Iaroslav Pyrogov <78590208+yarka-guru@users.noreply.github.com>
2026-02-10 22:59:13 +0100

    Update rds-ssm-connect to v1.7.13 with ARM64 Linux support

--- Formula/rds-ssm-connect.rb
+++ Formula/rds-ssm-connect.rb
@@ -1,13 +1,17 @@
 class RdsSsmConnect < Formula
   desc "Secure database tunneling via AWS SSM"
   homepage "https://github.com/yarka-guru/connection_app"
-  version "1.7.12"
+  version "1.7.13"
   license "MIT"
 
   on_linux do
     on_intel do
       url "https://github.com/yarka-guru/connection_app/releases/download/v#{version}/RDS.SSM.Connect_#{version}_amd64.deb"
-      sha256 "bf044df9baef760343b9797f4a806d3a5af06472719e7dc083f397d38ab54d3d"
+      sha256 "5ac56859fffd46dcd54359b167260852223b3daf413fee88721b29354ef8a302"
+    end
+    on_arm do
+      url "https://github.com/yarka-guru/connection_app/releases/download/v#{version}/RDS.SSM.Connect_#{version}_arm64.deb"
+      sha256 "e9c72d2a9c4c1d8fc0b65c7828b6f94afb08f0969626055fffd3406baf1b1c9c"
     end
   end
 
